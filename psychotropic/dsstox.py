dba29759a1611ffde5e2ed1f419fbc50713f81c9
x-yzt <xyzt@psychonaut.fr>
2022-01-17 16:26:05 +0100

    update solubility cmd to new comptox api

--- psychotropic/dsstox.py
+++ psychotropic/dsstox.py
@@ -1,41 +1,47 @@
-import json
-
 import httpx
-from bs4 import BeautifulSoup
 
 
-DSSTOX_URL = "https://comptox.epa.gov/dashboard/dsstoxdb/"
-
-PHYSICAL_PROPERTIES = [
-    'density',
-    'mv', # Molar volume
-    'mp', # Melting point
-    'bp', # Boiling point
-    'fp', # Flash point
-    'wsol',
-    'logkow',
-    'logkoa',
-    'polarizability',
-    'thermalconductivity',
-    'surface tension', 
-    'viscosity',
-    'ior', # Index of refraction
-    'mr', # Molar refractivity
-    'vp', # Vapor pressure
-    'hlc', # Henry's law constant
-]
+COMPTOX_URL = "https://comptox.epa.gov/dashboard-api/"
 
 
-async def get_properties(query):
+async def search_substances(query):
+    """Return a list of substances from the CompTox database that match the
+    query string.
+    """
+    query = query.lower()
 
-    async with httpx.AsyncClient(base_url=DSSTOX_URL, timeout=30) as client:
-        r = await client.get('results', params={'search': query})
-    
-    # The data to get is contained in this tag:
-    # <div id="single_results" data-result="HTML ESCAPED JSON DATA"></div>
-    soup = BeautifulSoup(r.text, 'html.parser')
-    tag = soup.find(id='single_results')
-    
-    if not tag:
-        return
-    return json.loads(tag.get('data-result'))
+    async with httpx.AsyncClient(base_url=COMPTOX_URL, timeout=30) as client:
+        r = await client.get('ccdapp1/search/chemical/start-with/' + query)
+
+    return r.json()
+
+
+async def get_substance(query):
+    """Query the CompTox database and return the best match."""
+    matches = await search_substances(query)
+    matches.sort(
+        key=lambda x: (x['rank'], x['searchWord'].lower() == query),
+        reverse=True
+    )
+    return matches[0] if len(matches) else None
+
+
+async def get_properties(dsstox_id):
+    """Query the CompTox database and return the DSSTox chemical properties of
+    a substance, given its DSSTox identifier.
+    """
+    async with httpx.AsyncClient(base_url=COMPTOX_URL, timeout=30) as client:
+        r = await client.get(
+            'ccdapp2/chemical-property/search/by-dtxsid',
+            params={'id': dsstox_id}
+        )
+    return r.json()
+
+
+def format_units(string):
+    """Replace ASCII exponents with Unicode chars."""
+    return (string
+        .replace('^2', '²')
+        .replace('^3', '³')
+        if string else ''
+    )
