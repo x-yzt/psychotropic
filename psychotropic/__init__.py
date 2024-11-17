4d8098e4a38d23f258c81b47995b65a56c979442
x-yzt <xyzt@psychonaut.fr>
2024-11-18 00:42:44 +0100

    add CLI arg to change log lvl

--- psychotropic/__init__.py
+++ psychotropic/__init__.py
@@ -1,3 +1,19 @@
+import logging
+from argparse import ArgumentParser
+
 import discord
 
-discord.utils.setup_logging()
+
+parser = ArgumentParser()
+parser.add_argument(
+    "-l",
+    "--log",
+    choices=logging._nameToLevel.keys(),
+    default="INFO",
+    help="Logging level."
+)
+
+log_level_name = parser.parse_args().log
+log_level = logging._nameToLevel.get(log_level_name)
+
+discord.utils.setup_logging(level=log_level)
