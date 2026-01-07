1c039b2e3363bc0dcebc4d2c0b089f14b1eaa06d
x-yzt <xyzt@psychonaut.fr>
2026-01-07 05:30:30 +0100

    localize now supports locale_str objects too + ruff fmt

--- psychotropic/i18n.py
+++ psychotropic/i18n.py
@@ -1,13 +1,11 @@
 import logging
 from contextvars import ContextVar
-from typing import Optional
 
 from babel.support import Translations
 from discord import Interaction, Locale
 from discord.app_commands import TranslationContext, Translator, locale_str
 
 from psychotropic import settings
-
 
 log = logging.getLogger(__name__)
 
@@ -20,8 +18,7 @@ class BabelTranslator(Translator):
 
         for locale in settings.TRANSLATIONS:
             self.translations[locale] = Translations.load(
-                settings.BASE_DIR / 'locales',
-                [locale]
+                settings.BASE_DIR / "locales", [locale]
             )
             log.info(f"Loaded translation catalog for locale: {locale}")
 
@@ -29,17 +26,13 @@ class BabelTranslator(Translator):
         self,
         string: locale_str,
         locale: Locale,
-        context: TranslationContext
-    ) -> Optional[str]:
+        context: TranslationContext,
+    ) -> str | None:
         return self.get_translation(string.message, str(locale))
 
-    def get_translation(
-        self,
-        string: str,
-        locale: str
-    ) -> Optional[str]:
-        """"Synchronous method to get translations outside of
-        discord.py's async translation system."""
+    def get_translation(self, string: str, locale: str) -> str | None:
+        """ "Synchronous method to get translations outside of discord.py's async
+        translation system."""
         catalog = self.translations.get(locale)
 
         if catalog is None:
@@ -51,25 +44,27 @@ class BabelTranslator(Translator):
 translator = BabelTranslator()
 
 
-current_locale = ContextVar('locale', default='en-US')
+current_locale = ContextVar("locale", default="en-US")
 
 
 def set_locale(obj: Interaction | str):
-    """Helper function to set the current locale to either a locale
-    string or to extract it from an Interaction object."""
+    """Helper function to set the current locale to either a locale string or to extract
+    it from an Interaction object."""
     if isinstance(obj, Interaction):
         current_locale.set(str(obj.locale))
     else:
         current_locale.set(obj)
 
 
-def localize(string: str) -> str:
-    """Helper function to translate bare strings according to current
-    context locale."""
+def localize(string: str | locale_str) -> str:
+    """Helper function to translate bare strings according to current context locale."""
+    if isinstance(string, locale_str):
+        string = string.message
+
     return translator.get_translation(string, current_locale.get()) or string
 
 
 def localize_fmt(string: str, /, **kwargs) -> str:
-    """Helper function to translate and format bare strings according
-    to current context locale."""
+    """Helper function to translate and format bare strings according to current context
+    locale."""
     return localize(string).format(**kwargs)
